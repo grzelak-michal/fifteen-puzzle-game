82e419ff2c7e25e39c33613fd3428ad57b33dc24
Michal Grzelak <grzelakm@outlook.com>
2019-11-07 21:22:19 +0100

    Test board logic

--- main.py
+++ main.py
@@ -17,5 +17,10 @@ def readBoard():
 
 
 
-board = readBoard()
-
+# board = readBoard()
+board = Board(np.arange(16).reshape(4, 4))
+print(board.is_solved())
+board.move('L')
+print(board.is_solved())
+print(board.board)
+print(board.move_history)
