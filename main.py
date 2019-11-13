ab7076c1134195e21ea8d84df75eeecca581b55c
Michal Grzelak <grzelakm@outlook.com>
2019-11-13 19:48:59 +0100

    Update main testing

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import numpy as np
 from board import Board
 import algorithms
 import time
+import heuristics
+from copy import deepcopy
 
 # TODO: implement checking for tile 0 and numbers in rows
 def readBoard():
@@ -21,29 +23,33 @@ def readBoard():
 
 # board = readBoard()
 arr = np.arange(16)
-# np.random.shuffle(arr)
+np.random.shuffle(arr)
 board = Board(arr.reshape(4, 4))
-board.move('L')
-board.move('L')
-board.move('L')
-board.move('U')
-board.move('U')
-board.move('U')
-board.move('R')
-board.move('R')
-board.move('D')
-board.move('D')
-board.move('D')
+# board.move('L')
+# board.move('L')
+# board.move('L')
+# board.move('U')
+# board.move('U')
+# board.move('U')
 # board.move('R')
+# board.move('R')
+# board.move('R')
+# board.move('D')
+# board.move('D')
+# board.move('D')
+# board.move('L')
+# board.move('L')
+board.move_history = []
 
 if board.is_solvable():
     print(board.board)
 
     start = time.time()
 
-    solved = algorithms.BFS(board, ['L', 'D', 'R', 'U'])
+    solved = algorithms.best_first_search(board, ['L', 'D', 'R', 'U'], heuristics.h3)
 
     end = time.time()
-    print(end - start)
+    print("Found solution in {} moves, time: {}".format(len(solved.move_history), end - start))
+
     print(solved.board)
 
